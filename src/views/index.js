12087a62cf5dab251cbff251b08a5574901fbc56
Jachen Duschletta <jcduschletta@gmail.com>
2018-02-08 08:44:27 -0800

    Initial Upload of Source Code
    added Source Code of the most current version

---
+++ src/views/index.js
@@ -0,0 +1,210 @@
+/* Global requirement */
+const ipcRenderer = require('electron').ipcRenderer
+const remote = require('electron').remote
+const fs = require('fs')
+
+/* Global Object to capture all columns and to save who is where by id */
+var Kanban = function () {
+  this.columns = [],
+  this.projects = [],
+  this.add = function(item) {
+    this.columns.push(item)
+  },
+  this.addProject = function(item) {
+    this.projects.push({item:item, parentID: "c0"})
+  },
+  this.movProject = function(idChild, idParent) {
+    for(let i=0; i < this.projects.length;i++) {
+      if(idChild == this.projects[i].item.id){
+        if(idParent){
+          console.log(`Changing ${this.projects[i].parentID} to`);
+          this.projects[i].parentID = idParent
+          console.log(`${this.projects[i].parentID} as parent`);
+        }
+      }
+    }
+  },
+  this.delProject = function(id) {
+    for(let i=0;i<this.projects.length;i++) {
+      if(id == this.projects[i].item.id){
+        console.log(`Found a match ${this.projects[i].item.id}`);
+        this.projects.splice(i,1)
+      }
+    }
+  },
+  this.render = function() {
+    var cont = document.getElementById("container")
+    cont.innerHTML = '' //clear all columns
+    this.columns.forEach( (el, ind) => {
+      var name = el.name
+      var id = el.id
+      var col = document.createElement("div")
+      col.setAttribute("class","column")
+      col.setAttribute("ondrop","onDrop(event)")
+      col.setAttribute("ondragover","onDragOver(event)")
+      col.setAttribute("id", id)
+      var title = document.createElement("div") //render name as title
+      title.setAttribute("class","title")
+      title.innerHTML = name
+      col.appendChild(title)
+      //check if a project is saved in this column, then append it
+      for(let i =0; i < this.projects.length;i++){
+        if(this.projects[i].parentID == id) {
+          let tempP = this.projects[i].item
+          let tempH = document.createElement("div")
+          tempH.setAttribute("class", "card")
+          tempH.setAttribute("draggable","true")
+          tempH.setAttribute("ondragstart","drag(event)")
+          tempH.setAttribute("ondblclick", "del(event)") /* Think about changing*/
+          tempH.setAttribute("id", tempP.id)
+          tempH.innerHTML = tempP.name + "<br>" + tempP.ord + "<br>" + tempP.contr
+          col.appendChild(tempH)
+        }
+      }
+      cont.appendChild(col)
+    })
+  }
+}
+
+const gkanban = new Kanban()
+
+/**
+* Project Object for purpose of saving and keeping track of
+* id = system tracker of id
+* name = Project name
+* ord = order number or other text
+* contr = contract date or other date
+*/
+var Project = function (id, name, ord, contr) {
+  this.id = "p" + id,
+  this.name = name,
+  this.ord = ord,
+  this.contr= contr
+}
+
+/**
+* Column Object for purposes of saving and keeping track of
+* id = system tracker of id
+* name = display name
+*/
+var Column = function (id, name) {
+  this.id = "c" + id,
+  this.name = name
+}
+
+/* Argument ID of DOM object to make visible */
+var makeVisi = function (id) {
+  var id = id
+  var field = document.getElementById(id)
+  field.style.display = "flex"
+}
+
+/* Argument ID of DOM object to make invisible (hide) */
+var makeInvisi = function (id) {
+  var id = id
+  var field = document.getElementById(id)
+  field.style.display = "none"
+}
+
+/**
+* Function to create new columns
+* Name from Input, New id from gKanban
+*/
+var newCol = function () {
+  makeInvisi('colM')
+  var name = document.getElementById("input")
+  var id = gkanban.columns.length
+  var col = new Column(id, name.value)
+  gkanban.add(col)
+  name.value = ''
+  gkanban.render()
+}
+
+/* Function to create new Project */
+var newProj = function () {
+  makeInvisi('projM')
+  var id = gkanban.projects.length
+  var name = document.getElementById("prj")
+  var ord = document.getElementById("ord")
+  var contr = document.getElementById("contr")
+  var contrd = parseDate(contr.value)
+  contrd = contrd.toDateString()
+  let proj = new Project(id, name.value, ord.value, contrd)
+  id.value = ''
+  ord.value = ''
+  contr.value = ''
+  gkanban.addProject(proj)
+  gkanban.render()
+}
+
+
+/* Parse Date Function */
+function parseDate (text) {
+  var b = text.split("-");
+  return new Date(b[0], --b[1], b[2]);
+}
+
+/* Drag and Drop Functions */
+
+function onDragOver(ev) {
+  ev.preventDefault()
+}
+
+function dropabble(ev) {
+ ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text/plain", ev.target.id)
+  ev.dataTransfer.dropEffect = "move"
+}
+
+function onDrop(ev) {
+  ev.preventDefault()
+  var data = ev.dataTransfer.getData("text/plain")
+  if(ev.target.getAttribute("draggable") != "true"){
+      ev.target.appendChild(document.getElementById(data))
+      gkanban.movProject(data, ev.target.id)
+  }
+}
+
+function del(ev) {
+  gkanban.delProject(event.target.id)
+  gkanban.render()
+}
+
+/* Loading and Saving data file */
+function backup() {
+  var data = JSON.stringify(gkanban)
+  console.log(`backed up: ${data}`);
+  fs.writeFileSync('kanbanBackup.pxx', data, 'utf8')
+  gkanban.render()
+}
+
+function loadBackup() {
+  fs.readFile('kanbanBackup.pxx', 'utf8', (err, data) => {
+    if(err){console.log(err)} //Handle Error so it doesn't stop execution
+    console.log(`Loaded from backup ${data}`);
+    var data = JSON.parse(data)
+    Object.assign(gkanban, data)
+    gkanban.render()
+  })
+}
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  fs.readFile('kanban.pxx', 'utf8', (err, data) => {
+    if(err){console.log(err)} //Handle Error so it doesn't stop execution
+    console.log(`Loaded ${data}`);
+    var data = JSON.parse(data)
+    Object.assign(gkanban, data)
+    gkanban.render()
+  })
+})
+
+window.addEventListener("beforeunload", (event) => {
+  var data = JSON.stringify(gkanban)
+  console.log(`Saved: ${data}`);
+  fs.writeFileSync('kanban.pxx', data, 'utf8')
+  return false
+})
